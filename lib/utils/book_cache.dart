55b515de483d8e88bc659febbf2e2bfb3f21fb29
haoyi <39499740@qq.com>
2021-07-05 10:32:10 +0800

    更新注释 增加缓存专用文件

---
+++ lib/utils/book_cache.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:yuedu/db/bookShelf.dart';
+
+class BookCache {
+  // static Future<void> cacheBook(BookInfo b) async {
+    
+  //   return 
+  // }
+  static Future<void> getContent(bookC)
+}
